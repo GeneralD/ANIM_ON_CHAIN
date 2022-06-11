b157d3934e2168444fd90e6bbf8dc3461815ff3a
GeneralD <yumejustice@gmail.com>
2022-06-11 18:32:17 +0900

    feat: add tests

--- test/testAJPBurn.ts
+++ test/testAJPBurn.ts
@@ -22,4 +22,33 @@ describe("Burn AJP", () => {
     expect(await instance.totalSupply()).to.equal(2)
     await expect(instance.ownerOf(3)).to.be.reverted
   })
+
+  it("Cannot burn same token twice", async () => {
+    const AJP = await ethers.getContractFactory("AJP")
+    const instance = await upgrades.deployProxy(AJP)
+
+    await instance.setMintLimit(10)
+    await instance.adminMint(5)
+
+    await instance.burn(2)
+    await expect(instance.burn(2)).to.reverted
+  })
+
+  it("Burning doesn't release the minting spaces", async () => {
+    const AJP = await ethers.getContractFactory("AJP")
+    const instance = await upgrades.deployProxy(AJP)
+
+    await instance.setMintLimit(5)
+    await instance.adminMint(5)
+
+    expect(await instance.totalSupply()).to.equal(5)
+
+    await instance.burn(1)
+    await instance.burn(2)
+    await instance.burn(3)
+
+    expect(await instance.totalSupply()).to.equal(2)
+    // burn reduces totalSupply but does't release space for future mint
+    await expect(instance.adminMint(5)).to.be.reverted
+  })
 })
