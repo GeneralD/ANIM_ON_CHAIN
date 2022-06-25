d8ca61c5bd087d71ac57e21683a279c220a28928
GeneralD <yumejustice@gmail.com>
2022-06-25 18:07:12 +0900

    feat: event testing

--- test/testAJPBurn.ts
+++ test/testAJPBurn.ts
@@ -12,14 +12,24 @@ describe("Burn AJP", () => {
     const instance = await upgrades.deployProxy(AJP) as AJP
 
     await instance.setMintLimit(10)
-    await instance.adminMint(5)
+    await expect(instance.adminMint(5))
+      .to.emit(instance, "Transfer")
+      .withArgs("0x0000000000000000000000000000000000000000", deployer.address, 5)
 
     expect(await instance.totalSupply()).to.equal(5)
     expect(await instance.ownerOf(3)).to.equal(deployer.address)
 
-    await instance.burn(1)
-    await instance.burn(2)
-    await instance.burn(3)
+    await expect(await instance.burn(1))
+      .to.emit(instance, "Transfer")
+      .withArgs(deployer.address, "0x0000000000000000000000000000000000000000", 1)
+
+    await expect(instance.burn(2))
+      .to.emit(instance, "Transfer")
+      .withArgs(deployer.address, "0x0000000000000000000000000000000000000000", 2)
+
+    await expect(instance.burn(3))
+      .to.emit(instance, "Transfer")
+      .withArgs(deployer.address, "0x0000000000000000000000000000000000000000", 3)
 
     expect(await instance.totalSupply()).to.equal(2)
     await expect(instance.ownerOf(3)).to.be.reverted
@@ -37,17 +47,30 @@ describe("Burn AJP", () => {
   })
 
   it("Burning doesn't release the minting spaces", async () => {
+    const [deployer] = await ethers.getSigners()
+
     const AJP = await ethers.getContractFactory("AJP")
     const instance = await upgrades.deployProxy(AJP) as AJP
 
     await instance.setMintLimit(5)
-    await instance.adminMint(5)
+
+    await expect(instance.adminMint(5))
+      .to.emit(instance, "Transfer")
+      .withArgs("0x0000000000000000000000000000000000000000", deployer.address, 5)
 
     expect(await instance.totalSupply()).to.equal(5)
 
-    await instance.burn(1)
-    await instance.burn(2)
-    await instance.burn(3)
+    await expect(await instance.burn(1))
+      .to.emit(instance, "Transfer")
+      .withArgs(deployer.address, "0x0000000000000000000000000000000000000000", 1)
+
+    await expect(instance.burn(2))
+      .to.emit(instance, "Transfer")
+      .withArgs(deployer.address, "0x0000000000000000000000000000000000000000", 2)
+
+    await expect(instance.burn(3))
+      .to.emit(instance, "Transfer")
+      .withArgs(deployer.address, "0x0000000000000000000000000000000000000000", 3)
 
     expect(await instance.totalSupply()).to.equal(2)
     // burn reduces totalSupply but does't release space for future mint
